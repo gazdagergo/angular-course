e5d4280fcb724cdf79ae04a9ebca64765e47db70
Gazda <gergely.gazda@accenture.com>
2017-04-03 13:09:42 +0200

    add component styles (inline)

--- src/app/favorite/favorite.component.ts
+++ src/app/favorite/favorite.component.ts
@@ -3,7 +3,11 @@ import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 @Component({
   selector: 'favorite',
   templateUrl: './favorite.component.html',
-  styleUrls: ['./favorite.component.css']
+  styles: [`
+    .glyphicon-star {
+      color: orange;
+    }
+  `]
 })
 export class FavoriteComponent implements OnInit {
 

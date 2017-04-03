e7b8d4facf26bbf8653f9d77fc120e677ff5ba7d
Gazda <gergely.gazda@accenture.com>
2017-04-03 12:45:21 +0200

    expose public property by alias

--- src/app/favorite/favorite.component.ts
+++ src/app/favorite/favorite.component.ts
@@ -1,14 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'favorite',
   templateUrl: './favorite.component.html',
-  styleUrls: ['./favorite.component.css'],
-  inputs: ['isFavorite']
+  styleUrls: ['./favorite.component.css']
 })
 export class FavoriteComponent implements OnInit {
 
-  isFavorite = false;
+  @Input('is-favorite') isFavorite = false;
 
   constructor() { }
 
